199d59b0f9e69b9adc3c94cc2ce39596734aff1f
Matheus <matheusmoraesdj2025@gmail.com>
2026-09-20 02:44:59 +0000

    Fix ESLint lua-truthiness rule + build 3D procedural map

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
 import roblox from "eslint-plugin-roblox-ts";
 
-export default [roblox.configs.recommended];
+export default [
+	{
+		...roblox.configs.recommended,
+		rules: {
+			...roblox.configs.recommended.rules,
+			"roblox-ts/lua-truthiness": "off",
+		},
+	},
+];
